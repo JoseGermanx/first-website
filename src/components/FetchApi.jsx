e15569b401e014fd2598391784b9b0f42fbc8492
Jose G Martinez <jgermanmm@gmail.com>
2024-02-13 09:02:26 -0300

    Actualizar FetchApi.jsx y todos.astro

--- src/components/FetchApi.jsx
+++ src/components/FetchApi.jsx
@@ -21,13 +21,17 @@ export default function FetchApi() {
     , [datos]);
 
     return (
-        <div>
+        <ul role="list" class="divide-y divide-gray-100 mx-auto">
             {loading ? "Cargando..." : datos.map(todo => (
-                <div key={todo.id}>
-                    <h3>{todo.title}</h3>
-                    <p>{todo.completed ? 'Completed' : 'Not Completed'}</p>
-                </div>
+                <li class="flex justify-between gap-x-6 py-5" key={todo.id}>
+                    <div class="flex min-w-0 gap-x-4">
+                    <div class="min-w-0 flex-auto">
+                    <h3 class="text-sm font-semibold leading-6 text-gray-900">{todo.title}</h3>
+                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">{todo.completed ? 'Completed' : 'Not Completed'}</p>
+                    </div>
+                    </div>
+                </li>
             ))}
-        </div>
+        </ul>
     );
 }
