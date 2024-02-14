22a81a6a8fc685ddae5fbe0a65116a27c9c47c76
Jose G Martinez <jgermanmm@gmail.com>
2024-02-14 02:02:59 -0300

    Agregada lógica para obtener todos desde el store y mostrarlos en la lista. Actualizado el estilo del footer.

--- src/components/FetchApi.jsx
+++ src/components/FetchApi.jsx
@@ -1,9 +1,12 @@
 import {useState, useEffect} from 'react';
 import { todos, fetchTodos  } from '../todosStore'
+import { useStore } from '@nanostores/react'
 
 export default function FetchApi() {
     const [datos, setDatos] = useState([]);
     const [loading, setLoading] = useState(true);
+
+    const $todos = useStore(todos);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos')
@@ -27,7 +30,7 @@ export default function FetchApi() {
 
     return (
         <ul role="list" class="divide-y divide-gray-100 mx-auto">
-            {loading ? "Cargando..." : datos.map(todo => (
+            {loading ? "Cargando..." : $todos.map(todo => (
                 <li class="flex justify-between gap-x-6 py-5" key={todo.id}>
                     <div class="flex min-w-0 gap-x-4">
                     <div class="min-w-0 flex-auto">
